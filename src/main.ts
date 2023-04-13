2373bf39da9fed68c604585e5902c657a8ef61c6
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-13 10:38:21 +0530

    feat: made express work

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
+import Express from "express"
 import * as dotenv from 'dotenv'
-import { Client } from 'guilded.js'
 
 dotenv.config()
 
-const TOKEN = process.env.GUILDED_TOKEN
+const app = Express()
 
-const client = new Client({token: TOKEN})
-
-client.on("ready", () => console.log(`Bot is successfully logged in`));
-	client.on("messageCreated", (message) => {
-			if (message.content === "test") {
-				    return message.reply("test indeed");
-			}
-	});
-
-client.login();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
