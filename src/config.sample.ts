d58b6f138b8c8b931dc3201aa29ce1b369ac752d
Saksham-Shahu <sakshamshahu.work@gmail.com>
2023-04-23 22:02:50 +0530

    feat: config file

---
+++ src/config.sample.ts
@@ -0,0 +1,2 @@
+export const client_id = "abc/xyz" //Enter the client id
+export const client_secret = "yourmom" //Enter the client secret
--- src/services/authlink.service.ts
+++ src/services/authlink.service.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import { URLSearchParams  } from "url";
-
+import { client_id } from "../config";
+import { client_secret } from "../config";
 export interface IGetToken {
     access_token: string;
     token_type: string;
@@ -18,23 +19,17 @@ export class Authlink {
     };
 
     public async exchangeInitialToken(code: string): Promise<IGetToken> {
-        const client_id = process.env.CLIENT_ID;
-        const client_secret = process.env.CLIENT_SECRET;
         const grant_type = "authorization_code";
         const response = await this._axios.post("token", new URLSearchParams({code, client_id, client_secret, grant_type}));
         return response.data;
     }
 
     public async refreshToken(refresh_token: string): Promise<IGetToken> {
-        const client_id = process.env.CLIENT_ID;
-        const client_secret = process.env.CLIENT_SECRET;
         const grant_type = "refresh_token";
         const response = await this._axios.post("token", new URLSearchParams({refresh_token, client_id, client_secret, grant_type}));
         return response.data;
     }
     public async revokeToken(refresh_token: string): Promise<void> {
-        const client_id = process.env.CLIENT_ID;
-        const client_secret = process.env.CLIENT_SECRET;
         await this._axios.post("revoke", new URLSearchParams({refresh_token, client_id, client_secret}));
         return;
     }
