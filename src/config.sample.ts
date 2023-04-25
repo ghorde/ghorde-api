37fa79e613802406f9eea89421e2ce488ea23f6a
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-25 14:00:17 +0530

    feat: https upgrader middleware

--- src/config.sample.ts
+++ src/config.sample.ts
@@ -1,3 +1,2 @@
 export const client_id = "abc/xyz"; //Enter the client id
 export const client_secret = "yourmom"; //Enter the client secret
-export const pem_passphrase = "yourmom"; //Enter the pem passphrase
--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,9 @@ import * as dotenv from "dotenv";
 import { configureDb, configureLogger } from "./helpers";
 import { LevelWithSilent } from "pino";
 import router from "./routers";
-// import https from 'https'
-// import fs from 'fs'
 import { httpser } from "./middleware/httpser.middleware";
 import path from "path";
-// import { pem_passphrase } from "./config";
 
-// const options = {
-//   key: fs.readFileSync(path.resolve('key.pem')),
-//   cert: fs.readFileSync(path.resolve('cert.pem')),
-//   passphrase: pem_passphrase
-// }
 
 dotenv.config();
 export const mainLogger = configureLogger(
@@ -41,7 +33,7 @@ configureDb(db, SURREAL_USER, SURREAL_PASS); // instance db
 
 const app = Express();
 app.use(Express.json());
-// app.use(httpser) not necessary rn
+app.use(httpser);
 app.use(Express.static(path.join(__dirname, 'public')));
 app.use(router);
 
