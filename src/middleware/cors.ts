317c1c3a61d856474ce72f5e8146468a766a9238
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-27 10:46:29 +0530

    fix: added options to allowed methods

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,6 +2,5 @@ import { originsList } from "../main";
 
 export const corsConfig = {
   origin: originsList,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization", "content-type"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 };
