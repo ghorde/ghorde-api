f07e18359cca4bb667f536f8e8d73bf8c180aeb0
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-27 10:27:38 +0530

    fix: cors content type

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,5 +3,4 @@ import { originsList } from "../main";
 export const corsConfig = {
   origin: originsList,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 };
