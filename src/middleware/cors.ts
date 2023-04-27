dbe9bafa55fb02be22fe8ff36f2f97943c44c8ec
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-27 10:55:41 +0530

    fix: added content-type to allowed headers

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,5 +4,5 @@ export const corsConfig = {
   origin: originsList,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization"]
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization", "content-type"],
 };
