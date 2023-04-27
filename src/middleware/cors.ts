89971fd29df4c7369820d8253d02d81045c11f54
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-27 10:52:14 +0530

    feat: allowed headers

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,5 +2,7 @@ import { originsList } from "../main";
 
 export const corsConfig = {
   origin: originsList,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization"]
 };
