601a6e1d8cd7dd83059e0aadc65375b5b95565f0
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-28 06:58:07 +0530

    feat: added models route for webclient

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -5,4 +5,5 @@ export const corsConfig = {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization"],
+  exposedHeaders: ["Content-Type"]
 };
--- src/routers/webclient/index.ts
+++ src/routers/webclient/index.ts
@@ -1,3 +1,4 @@
+import { shGetModels } from "../../controllers/stablehorde/stablehorde.controller";
 import { default as session } from "./session.routers";
 import { default as user } from "./user.routers";
 import { Router } from "express";
@@ -6,5 +7,6 @@ const router = Router();
 
 router.use("/session", session);
 router.use("/user", user);
+router.route("/models").get(shGetModels);
 
 export default router;
