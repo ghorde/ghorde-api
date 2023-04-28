d0da21dd977602affc2f5fb2a9f988d36d48905a
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-28 08:32:41 +0530

    test: allowed all origins

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import { originsList } from "../main";
 
 export const corsConfig = {
-  origin: originsList,
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization"],
