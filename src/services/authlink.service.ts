296433f5f2ef390940fe159c60c807cf591b032b
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-30 08:41:06 +0530

    fix: accounted for separate containers for authlink and ghorde api

--- src/services/authlink.service.ts
+++ src/services/authlink.service.ts
@@ -14,7 +14,7 @@ class Authlink {
   private _baseurl: string;
   private _axios: AxiosInstance;
   constructor() {
-    this._baseurl = "http://authlink/api/v1/";
+    this._baseurl = "http://ghorde-authlink.kodski.com:3020/api/v1/";
     this._axios = axios.create({ baseURL: this._baseurl });
   }
 
